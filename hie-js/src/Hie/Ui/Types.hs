37997b616c19326dd432f53c4c176760709b1d8a
Philip Lykke Carlsen <plcplc@gmail.com>
2016-10-10 20:46:00 +0200

    Phased out dynamic hie-ui

--- hie-js/src/Hie/Ui/Types.hs
+++ hie-js/src/Hie/Ui/Types.hs
@@ -30,91 +30,6 @@ import Data.Constraint
 import Data.Proxy
 import qualified Data.Map as M
 
-  {-
-pattern Proj x <- (proj -> Just x)
-
-type LookupUi uidomain t m = 
-  Term uidomain -> 
-  (PolyDyn, Event t PolyDyn) ->
-  (String -> Dynamic t (Maybe (HieValue uidomain))) ->
-  Maybe (m (HieSessionActions t uidomain, Event t PolyDyn))
-
-data UiImpl uidomain t m where
-  UiImpl ::
-    (
-      Typeable (sig :: *),
-      ui :<: uidomain,
-      MonadWidget t m,
-      Functor m,
-      Reflex t
-    ) =>
-    UiImplK t uidomain m ui sig ->
-    UiImpl uidomain t m
-
-type UiImplK t uidomain m ui sig =
-  (String -> Dynamic t (Maybe (HieValue uidomain))) ->
-  LookupUi uidomain t m ->
-  ui (Term uidomain) ->
-  Dynamic t (PolyMatch sig) ->
-  m (HieSessionActions t uidomain, Event t (PolyMatch sig))
-
-instance (Reflex t) => Monoid (HieSessionActions t uidomain) where
-  mempty = HieSessionActions never
-  mappend bs1 bs2 = mconcat [bs1, bs2]
-  mconcat bs = HieSessionActions $ mconcat (map updateBindings bs)
-
-noAction :: Reflex t => HieSessionActions t uidomain
-noAction = HieSessionActions never
-type UiEnv uidomain t m = [UiImpl uidomain t m]
-
-data HieValue uidomain =
-  HieValue {
-    hieValue :: PolyDyn,
-    hieUi    :: Term uidomain
-    }
-
--- | A data type expressing a grammar of 'f'-expression trees, represented by
--- lazy potentially infinite syntax tree of all the expressions permitted by the
--- grammar.
-newtype UiGrammar f = UiGrammar { unGrammar :: [f (UiGrammar f)] }
-
-allGrammar :: [UiGrammar f -> f (UiGrammar f)] -> UiGrammar f
-allGrammar fs = UiGrammar (map ($ allGrammar fs) fs)
-
--- | Applicative interface to Dynamic values. Used by the 'uiSelector'. This is
--- necessary for traversals (using 'traverse') that generate 'Dynamic's.
-newtype UiBuilder m t a = UiBuilder {unUiBuilder :: m (Dynamic t a)}
-
-instance (MonadHold t m, Reflex t) => Functor (UiBuilder m t) where
-  fmap f (UiBuilder m) = UiBuilder $ mapDyn f =<< m
-  x <$ _ = UiBuilder $ return $ constDyn x
-
-instance (MonadHold t m, Reflex t) => Applicative (UiBuilder m t) where
-  pure = UiBuilder . return . constDyn
-  (UiBuilder f) <*> (UiBuilder x) = UiBuilder $ do
-    f' <- f
-    x' <- x
-    mapDyn (\(f'', x'') -> f'' x'') =<< collectDyn (f', x')
-
-class UiSelectable (ui :: * -> *) where
-  uiIdentifier :: ui a -> String
-  enumerateUi  :: [Const ui]
-
-instance (
-  ui1 :<: (ui1 :+: ui2),
-  ui2 :<: (ui1 :+: ui2),
-  UiSelectable ui1,
-  UiSelectable ui2
-  ) =>
-  UiSelectable (ui1 :+: ui2) where
-
-  uiIdentifier = caseF uiIdentifier uiIdentifier
-
-  enumerateUi =
-    (map inj (enumerateUi :: [Const ui1])) ++
-    (map inj (enumerateUi :: [Const ui2]))
-  -}
-
 
 data HieSessionActions t caps =
   HieSessionActions {
@@ -128,7 +43,7 @@ data CrudAction caps =
 -- | Data type to identify hie values. Usable for UI in eg. function application
 -- and reactive updates.
 data HieRef = HieRef Integer
-  -- TODO cases for unique opaque id, friendly unique name, and...?
+  -- TODO? cases for unique opaque id, friendly unique name, and...?
   deriving (Eq, Ord, Show)
 
 -- * About the structure of a hie document.
@@ -151,7 +66,6 @@ data HieRef = HieRef Integer
 class HieDocument v where
   -- methods to localise and manipulate nested values.
   nestedValues :: v -> M.Map HieRef (Dynamic t (HieValue caps))
-
 
 class MaybeInstance (cap :: * -> Constraint) a where
   maybeInstance :: Proxy cap -> Proxy a -> Maybe (Dict (cap a))
